2a9391942393340e0870b09b2198d0899382a41c
Jeffrey Morse <jam40jeff@gmail.com>
2014-10-10 10:45:16 -0400

    Sign assembly.

--- Source/MorseCode.ITask/_Root/Properties/AssemblyInfo.cs
+++ Source/MorseCode.ITask/_Root/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_Root")]
+[assembly: AssemblyTitle("ITask")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_Root")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("MorseCode Software")]
+[assembly: AssemblyProduct("MorseCode ITask")]
+[assembly: AssemblyCopyright("Copyright © MorseCode Software 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
