0da46930d0609d289cf007bf99dddfd2471c5129
Jeffrey Morse <jam40jeff@gmail.com>
2015-03-09 00:33:53 -0400

    Add TaskExtensionMethods and nuspec.

--- Source/MorseCode.ITask/_Root/TaskExtensionMethods.cs
+++ Source/MorseCode.ITask/_Root/TaskExtensionMethods.cs
@@ -64,11 +64,42 @@ namespace MorseCode.ITask
         /// The type of the result of the task.
         /// </typeparam>
         /// <returns>
-        /// The <see cref="ITask"/>.
+        /// The <see cref="ITask{TResult}"/>.
         /// </returns>
         public static ITask<TResult> AsITask<TResult>(this Task<TResult> task)
         {
             return new TaskWrapper<TResult>(task);
+        }
+
+        /// <summary>
+        /// Converts the <see cref="ITask"/> instance into a <see cref="Task"/>.
+        /// </summary>
+        /// <param name="task">
+        /// The task to convert.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public static async Task AsTask(this ITask task)
+        {
+            await task.ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Converts the <see cref="ITask{TResult}"/> instance into a <see cref="Task{TResult}"/>.
+        /// </summary>
+        /// <param name="task">
+        /// The task to convert.
+        /// </param>
+        /// <typeparam name="TResult">
+        /// The type of the result of the task.
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task{TResult}"/>.
+        /// </returns>
+        public static async Task<TResult> AsTask<TResult>(this ITask<TResult> task)
+        {
+            return await task;
         }
 
         /// <summary>
