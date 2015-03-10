1f0061ffdbc6798caa716b276929a0178aca8734
Jeffrey Morse <jam40jeff@gmail.com>
2015-03-10 07:40:34 -0400

    Fix nuspec.

--- Source/MorseCode.ITask/_Root/Properties/AssemblyInfo.cs
+++ Source/MorseCode.ITask/_Root/Properties/AssemblyInfo.cs
@@ -1,16 +1,47 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region License
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="MorseCode Software">
+// Copyright (c) 2015 MorseCode Software
+// </copyright>
+// <summary>
+// The MIT License (MIT)
+// 
+// Copyright (c) 2015 MorseCode Software
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ITask")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MorseCode ITask")]
+[assembly: AssemblyDescription("Provides an awaitable covariant ITask interface which may be used in place of the built-in Task class.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MorseCode Software")]
 [assembly: AssemblyProduct("MorseCode ITask")]
-[assembly: AssemblyCopyright("Copyright © MorseCode Software 2014")]
+[assembly: AssemblyCopyright("Copyright © MorseCode Software 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
